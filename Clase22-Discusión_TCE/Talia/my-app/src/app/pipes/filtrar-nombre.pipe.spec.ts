a8c335e3750ccae409dd3f09bebb3a76e912f427
Marina <almaralesmarina24@gmail.com>
2024-01-19 11:27:38 -0500

    Actualizando repositorio

--- Clase22-Discusión_TCE/Talia/my-app/src/app/pipes/filtrar-nombre.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltrarNombrePipe } from './filtrar-nombre.pipe';
-
-describe('FiltrarNombrePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltrarNombrePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
